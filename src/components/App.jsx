fc29aba6b210f08b7baaadbe710406096cda0ad5
rga-king <rga.king@gmail.com>
2025-10-30 11:56:38 +0000

    refactor data structures

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,19 @@
 import { useState } from 'react';
 import Header from './Header.jsx';
-// import Main from './Main.jsx';
 import Footer from './Footer.jsx';
 import Textarea from './Textarea.jsx';
 import Stats from './Stats.jsx';
-import {
-  INSTAGRAM_MAX_CHARACTERS,
-  X_FREE_MAX_CHARACTERS
-} from '../lib/constants.js';
+import { socialMediaStats } from '../data/socialMediaData.js';
 
 function App() {
   const [text, setText] = useState('');
-  const stats = [
-    // {
-    //   label: 'Words',
-    //   value: text.split(' ')[0] === '' ? 0 : text.split(' ').length
-    // },
-    // { label: 'Characters', value: text.length },
-    {
-      label: 'post/reel caption',
-      value: INSTAGRAM_MAX_CHARACTERS - text.length
-    },
-    {
-      label: 'X free user account',
-      value: X_FREE_MAX_CHARACTERS - text.length
-    },
-    {
-      label: 'Instagram post/reel caption',
-      value: INSTAGRAM_MAX_CHARACTERS - text.length
-    },
-    {
-      label: 'X free user account',
-      value: X_FREE_MAX_CHARACTERS - text.length
-    }
-  ];
 
   return (
     <>
       <Header />
       <main className='container'>
         <Textarea text={text} setText={setText} />
-        <Stats stats={stats} />
+        <Stats stats={socialMediaStats} charCount={text.length} />
       </main>
       <Footer />
     </>
